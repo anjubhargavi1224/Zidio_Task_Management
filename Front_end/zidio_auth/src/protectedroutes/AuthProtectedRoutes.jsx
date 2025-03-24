872678f7053ebfab1436d7cb1c96d3b62329cc9d
Anandhukb0007 <anandhukb0007@gmail.com>
2025-03-24 23:03:38 +0530

    Add AuthProtectedRoutes component for route protection based on authentication token

---
+++ Front_end/zidio_auth/src/protectedroutes/AuthProtectedRoutes.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const AuthProtectedRoutes = () => {
+    const token = localStorage.getItem("token"); 
+
+   
+
+  return token ?  <Outlet/> : <Navigate to="/" />;
+}
+
+export default AuthProtectedRoutes
